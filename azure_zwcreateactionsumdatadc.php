159d183c2224403193a8ee709f566d6182694744
yuqinyi <yuqinyi@hotmail.com>
2017-06-02 09:21:53 +0800

    azure schedule.234

---
+++ azure_zwcreateactionsumdatadc.php
@@ -0,0 +1,23 @@
+<?php
+
+$connectionOptions = array(
+    'Database' => "mimamoriDB2",
+    'Uid' => "sch001",
+    'PWD' => "Passw0rd",
+    'CharacterSet' => 'UTF-8'
+);
+$conn = sqlsrv_connect("tcp:mimamori.database.windows.net,1433", $connectionOptions);
+
+//调用存储过程，并填充参数
+$callSP = " [sch001].[zwcreateactionsumdatadc] ";
+
+$stmt = sqlsrv_query($conn, $callSP);
+
+if ($stmt === false) {
+    echo "Error in executing statement 3.\n";
+    die(print_r(sqlsrv_errors(), true));
+}
+
+sqlsrv_free_stmt($stmt);
+
+?>
